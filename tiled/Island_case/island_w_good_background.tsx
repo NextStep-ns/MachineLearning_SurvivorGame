d0b311a945834842127e5d8471c546442872f994
Charles Descamps <94001828+M-ister-IO@users.noreply.github.com>
2024-05-17 18:05:36 +0200

    V1.4 - images and map (no obstacles)

---
+++ tiled/Island_case/island_w_good_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="beach_tileset" tilewidth="16" tileheight="16" tilecount="936" columns="36">
+ <image source="tsx/beach_tileset.png" trans="ffffff" width="576" height="416"/>
+</tileset>
---
+++ tiled/Island_case/ocean.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="ocean" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="tf_beach_tileA1.png" trans="000000" width="512" height="384"/>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="250"/>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="6" duration="250"/>
+   <frame tileid="36" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="250"/>
+   <frame tileid="14" duration="250"/>
+   <frame tileid="13" duration="250"/>
+   <frame tileid="12" duration="250"/>
+   <frame tileid="11" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="193">
+  <animation>
+   <frame tileid="193" duration="250"/>
+   <frame tileid="195" duration="250"/>
+   <frame tileid="197" duration="250"/>
+   <frame tileid="231" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="523">
+  <animation>
+   <frame tileid="523" duration="250"/>
+   <frame tileid="520" duration="250"/>
+   <frame tileid="518" duration="250"/>
+   <frame tileid="519" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="619">
+  <animation>
+   <frame tileid="619" duration="250"/>
+   <frame tileid="678" duration="250"/>
+   <frame tileid="679" duration="250"/>
+   <frame tileid="619" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
