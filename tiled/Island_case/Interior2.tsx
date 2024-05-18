07e2a133524c532726660b59cb799d7602d00caa
Charles Descamps <94001828+M-ister-IO@users.noreply.github.com>
2024-05-18 22:31:55 +0200

    V1 - picks up carrots, sometimes blinks

---
+++ tiled/Island_case/Interior2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Interior" tilewidth="10" tileheight="10" tilecount="4489" columns="67">
+ <image source="Pixel_Interiors/Interior.png" trans="000000" width="675" height="672"/>
+</tileset>
---
+++ tiled/Island_case/NatureTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="NatureTiles" tilewidth="16" tileheight="16" tilecount="504" columns="24">
+ <image source="NatureTiles.png" trans="000000" width="384" height="336"/>
+</tileset>
